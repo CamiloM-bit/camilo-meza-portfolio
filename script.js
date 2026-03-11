ccaeb6bd3fba785f82e3e121e1919b56c11c224b
CamiloM-bit <mezacamilo841@gmail.com>
2026-03-11 16:50:26 -0500

    feat: agregar botón flotante para cambiar modo claro/oscuro

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
     const btnDarkLight = document.querySelector('.btn-dark-light');
+    const darklighticon = document.querySelector('.dark-light-icon');
 
     const savedDarkMode = localStorage.getItem('darkMode') === 'true';
     if (savedDarkMode) {
         document.body.classList.add('dark-mode');
+        darklighticon.classList.add('dark')
     }
   
     btnDarkLight.addEventListener('click', () => {
         document.body.classList.toggle('dark-mode');
+        darklighticon.classList.toggle('dark')
         
         const isDarkMode = document.body.classList.contains('dark-mode');
         localStorage.setItem('darkMode', isDarkMode);
